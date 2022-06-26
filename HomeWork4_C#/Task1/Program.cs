1e915b5ad0c2994925536f04a922cc3665af58ea
Sergey <sergeirabotaf@mail.ru>
2022-06-26 22:36:52 +0500

    add homework

---
+++ HomeWork4_C#/Task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    string param = Console.ReadLine();
+    return int.Parse(param);
+}
+
+int n = Prompt("Введите число: ");
+int result = 0;
+
+while (n > 0)
+{
+    result += n % 10;
+    n /= 10;
+}
+
+System.Console.WriteLine($"Сумма цифр в введенном числе равна: {result}.");
