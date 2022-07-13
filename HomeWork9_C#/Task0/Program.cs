0c7b74b90d0ea78869ad35af086cb25870402b8d
Sergey <sergeirabotaf@mail.ru>
2022-07-13 20:04:51 +0500

    add homework

---
+++ HomeWork9_C#/Task0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задайте значения M и N. Напишите программу, которая выведет 
+//все чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintEvenNumbersBetweenMN(int n, int m)
+{
+    if (n < m) return;
+    PrintEvenNumbersBetweenMN(n - 1, m);
+    if (n % 2 == 0) System.Console.Write(n + ", ");
+}
+
+
+int numberM = Prompt("Введите число M: ");
+int numberN = Prompt("Введите число N: ");
+
+PrintEvenNumbersBetweenMN(numberN, numberM);
