30c09f3e77be30bee477a46b2161ce223bae295a
Sergey <sergeirabotaf@mail.ru>
2022-07-24 22:35:12 +0500

    add homework

---
+++ HomeWork10_C#/Task0/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 1: Есть монотонная последовательность, каждое число встречается ровно то количество раз, 
+// какое значение этого числа: 1, 2, 2, 3, 3, 3, 4, 4, 4, 4,... Выведите ряд этой последовательности до N-го значения, желательно использовать рекурсию:
+// N = 12 -> "1, 2, 2, 3, 3, 3, 4, 4, 4, 4, 5, 5"
+
+
+int Prompt(string message)
+{
+    System.Console.Write(message);
+    return int.Parse(Console.ReadLine());
+}
+
+void PrintMonoSequence(int n)
+{
+    if (n == 0) return;
+    PrintMonoSequence(n - 1);
+    for (int i = 1; i <= n; i++)
+    {
+        System.Console.Write(n + ", ");
+    }
+}
+
+int number = Prompt("Введите число N: ");
+PrintMonoSequence(number);
